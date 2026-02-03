6d76ea760fb1a9d618c1764e1ea332df88f57dd7
Subhash Bhushan <494533+subhashb@users.noreply.github.com>
2026-02-02 20:27:30 -0800

    Prepare database in Github Actions for test run

--- src/identity/create_db.sh
+++ src/identity/create_db.sh
@@ -11,15 +11,15 @@ DB_NAME="identity_local"
 DB_USER="identity_local"
 DB_USER_PASSWORD="identity_local"
 
-# SQL commands to create the user
+# SQL commands to create the user (idempotent)
 SQL_CREATE_USER=$(cat <<EOF
-CREATE USER $DB_USER WITH PASSWORD '$DB_USER_PASSWORD';
-EOF
-)
-
-# SQL commands to create the database
-SQL_CREATE_DB=$(cat <<EOF
-CREATE DATABASE $DB_NAME WITH OWNER $DB_USER;
+DO \$\$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '$DB_USER') THEN
+        CREATE USER $DB_USER WITH PASSWORD '$DB_USER_PASSWORD';
+    END IF;
+END
+\$\$;
 EOF
 )
 
@@ -36,8 +36,10 @@ EOF
 # Execute the SQL command to create the user
 PGPASSWORD="$DB_ADMIN_PASSWORD" psql -v ON_ERROR_STOP=1 -h "$DB_HOST" -p "$DB_PORT" -U "$DB_ADMIN" -d postgres -c "$SQL_CREATE_USER"
 
-# Execute the SQL command to create the database separately
-PGPASSWORD="$DB_ADMIN_PASSWORD" psql -v ON_ERROR_STOP=1 -h "$DB_HOST" -p "$DB_PORT" -U "$DB_ADMIN" -d postgres -c "$SQL_CREATE_DB"
+# Create the database if it doesn't exist
+if ! PGPASSWORD="$DB_ADMIN_PASSWORD" psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_ADMIN" -lqt | cut -d \| -f 1 | grep -qw "$DB_NAME"; then
+    PGPASSWORD="$DB_ADMIN_PASSWORD" psql -v ON_ERROR_STOP=1 -h "$DB_HOST" -p "$DB_PORT" -U "$DB_ADMIN" -d postgres -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER;"
+fi
 
 # Execute the SQL commands to grant schema-level privileges by connecting to the newly created database
 PGPASSWORD="$DB_ADMIN_PASSWORD" psql -v ON_ERROR_STOP=1 -h "$DB_HOST" -p "$DB_PORT" -U "$DB_ADMIN" -d "$DB_NAME" -c "$SQL_GRANT_PRIVILEGES"
